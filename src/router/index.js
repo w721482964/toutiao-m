aba923d09d624910fe7304bed8d0de549537b7f7
ttai <721482964@qq.com>
2020-12-14 12:58:01 +0800

    项目初始化完成

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,38 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const routes = [
-
+  {
+    path: '/login',
+    name: 'login',
+    // 路由懒加载 和之前做法没区别
+    component: () => import('@/views/login/')
+  },
+  {
+    path: '/',
+    component: () => import('@/views/layout/'),
+    children: [
+      {
+        path: '', // 默认子路由
+        name: 'home',
+        component: () => import('@/views/home/')
+      },
+      {
+        path: '/qa',
+        name: 'qa',
+        component: () => import('@/views/qa/')
+      },
+      {
+        path: '/my',
+        name: 'my',
+        component: () => import('@/views/my/')
+      },
+      {
+        path: '/video',
+        name: 'video',
+        component: () => import('@/views/video/')
+      }
+    ]
+  }
 ]
 
 const router = new VueRouter({
---
+++ src/views/login/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="login-container">aaa</div>
+</template>
+
+<script>
+export default {
+  name: 'LoginIndex'
+}
+</script>
+
+<style>
+</style>
